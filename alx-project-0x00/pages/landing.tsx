36393e5e948ffd81ec669389fdba4200309d6e9a
pdanyomiportfolio <anyomiprince27@gmail.com>
2025-09-17 21:38:05 +0000

    Basic Routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,10 @@
+// pages/landing.tsx
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
